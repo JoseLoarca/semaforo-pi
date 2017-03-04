29d44f0682f0ade256cef4c973e6dab8d5dac779
Jose Loarca <joseloarca97@icloud.com>
2017-03-04 08:04:58 +0000

    basic traffic lights sequence done

--- trafficlights.py
+++ trafficlights.py
@@ -1,13 +1,27 @@
 from gpiozero import Button, TrafficLights, Buzzer
+from time import sleep
 
 button = Button(21)
 lights = TrafficLights(25, 8, 7)
 buzzer = Buzzer(15)
 
 while True:
-    lights.blink()
-    buzzer.beep()
-    button.wait_for_press()
-    lights.off()
-    buzzer.off()
-    button.wait_for_release()
+        buzzer.beep(1)
+        lights.green.on()
+        sleep(20)
+        buzzer.beep(0.5, 0.5)
+        lights.green.blink(0.5, 0.5)
+        sleep(3)
+        lights.green.off()
+        buzzer.beep(1)
+        lights.amber.on()
+        sleep(2)
+        buzzer.beep(0.5, 0.5)
+        lights.amber.blink(0.5, 0.5)
+        sleep(3)
+        lights.amber.off()
+        buzzer.beep(1)
+        lights.red.on()
+        sleep(20)
+        lights.red.off()
+        
